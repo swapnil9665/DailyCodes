a6b3a91eff82984425505fb5d7969bdb2ea4f2bb
swapnil965 <tambe.swapnil108@gmail.com>
2025-07-08 00:00:37 +0530

    string demo

---
+++ StringDemo.java
@@ -0,0 +1,13 @@
+public class StringDemo{
+    public static void main(String[] args) {
+        String str = "mahendra";
+        System.out.println(str.length());
+        int i[] = {2,3,5};
+        System.out.println(i.length);
+        char ch[] = str.toCharArray();
+        System.out.println(ch);
+
+        System.out.println(ch[3]);
+        
+    }
+}
